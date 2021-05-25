0685e546462fb7d2d9fbec528767e62531eb63a8
Shubhankar Debnath <56063427+Pentanest@users.noreply.github.com>
2021-05-25 20:30:29 +0530

    1st Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Spacemaker.css'
+import LandingPage from '../src/components/LandingPage'
+
+function App() {
+  return (
+    <>
+    <LandingPage />
+    </>
+  )
+}
+
+export default App;
