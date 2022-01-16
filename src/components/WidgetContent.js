243d346d01b04d14954d6a9ba2f883d02316affd
Shubhankar Debnath <56063427+Pentanest@users.noreply.github.com>
2022-01-16 13:19:23 +0530

    Added new files

---
+++ src/components/WidgetContent.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./WidgetContent.css";
+
+function WidgetContent() {
+  return (
+    <div className="widget__contents">
+      <div className="widget__content">
+        <img
+          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1737435-100-jxcfmjdvwvpkcketifttdmeeimxcatua.jpeg"
+          alt=""
+        />
+        <div className="widget__contentTitle">
+          <h5>Enactus</h5>
+          <p>Enterpreneur Developement</p>
+        </div>
+      </div>
+      <div className="widget__content">
+        <img
+          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1574818-100-mzdwostcualpwcxekyrvyqqpychetdoc.jpeg"
+          alt=""
+        />
+        <div className="widget__contentTitle">
+          <h5>Rotaract Club</h5>
+          <p>College Events Handling</p>
+        </div>
+      </div>
+      <div className="widget__content">
+        <img
+          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1644613-100-ydflucgoeztbhwyurtmlqqrgfqmjmhpl.jpeg"
+          alt=""
+        />
+        <div className="widget__contentTitle">
+          <h5>Art & Artist</h5>
+          <p>Regulated by Prof. ABC</p>
+        </div>
+      </div>
+      <div className="widget__content">
+        <img
+          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1647318-100-kmwvqbpzatmylibelrowrerfqbspekwo.jpeg"
+          alt=""
+        />
+        <div className="widget__contentTitle">
+          <h5>Nukkad Naatak</h5>
+          <p>Enact your way</p>
+        </div>
+      </div>
+      <div className="widget__content">
+        <img
+          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1578647-100-xkggvbyzfkvzhyklewtkjijefekqbazb.jpeg"
+          alt=""
+        />
+        <div className="widget__contentTitle">
+          <h5>Stock Market Strategies</h5>
+          <p>Everything about investing in Stock...</p>
+        </div>
+      </div>
+      <div className="widget__content">
+        <img
+          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1711686-100-glvgnbatdxpjbhrkyphlfamqrryfccvp.jpeg"
+          alt=""
+        />
+        <div className="widget__contentTitle">
+          <h5>Architecture World</h5>
+          <p>All about civil architecture...</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WidgetContent;
